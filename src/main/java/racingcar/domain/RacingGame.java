2291b8b00555ca3d496658e6c45ccde29a116090
San <aeginventor@gmail.com>
2025-10-27 19:33:49 +0900

    feat: 1회 경주 실행(raceOneRound) 기능 구현

--- src/main/java/racingcar/domain/RacingGame.java
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -1,10 +1,15 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class RacingGame {
+
+    private static final int MIN_RANDOM_NUMBER = 0;
+    private static final int MAX_RANDOM_NUMBER = 9;
 
     private final List<Car> cars;
 
@@ -17,5 +22,12 @@ public class RacingGame {
     public List<Car> getCars() {
         // 외부에서 cars 리스트를 수정하는 것을 방지 (e.g., cars.add(...))
         return Collections.unmodifiableList(cars);
+    }
+
+    public void raceOneRound() {
+        for (Car car : cars) {
+            int randomNumber = Randoms.pickNumberInRange(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER);
+            car.move(randomNumber);
+        }
     }
 }

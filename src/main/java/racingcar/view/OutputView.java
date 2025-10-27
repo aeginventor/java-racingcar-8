4482a7047288e9fd77d684330120857e1b66db77
San <aeginventor@gmail.com>
2025-10-27 20:12:40 +0900

    refactor: OutputView 매직 스트링 상수화
    하드코딩된 문자열(매직 스트링)을 private static final 상수로 추출

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -7,15 +7,21 @@ import java.util.stream.Collectors;
 
 public class OutputView {
 
+    private static final String HEADER_EXECUTION_RESULT = "\n실행 결과";
+    private static final String CAR_POSITION_FORMAT = " : ";
+    private static final String WINNER_ANNOUNCEMENT_PREFIX = "최종 우승자 : ";
+    private static final String WINNER_DELIMITER = ", ";
+    private static final String ERROR_PREFIX = "[ERROR] ";
+
     public void printExecutionResultHeader() {
-        System.out.println("\n실행 결과");
+        System.out.println(HEADER_EXECUTION_RESULT);
     }
 
     public void printRoundResult(List<Car> cars) {
         for (Car car : cars) {
             String carName = car.getName();
             String positionDisplay = car.getPositionDisplay();
-            System.out.println(carName + " : " + positionDisplay);
+            System.out.println(carName + CAR_POSITION_FORMAT + positionDisplay);
         }
         System.out.println();
     }
@@ -23,12 +29,12 @@ public class OutputView {
     public void printWinners(List<Car> winners) {
         String winnerNames = winners.stream()
                 .map(Car::getName)
-                .collect(Collectors.joining(", "));
+                .collect(Collectors.joining(WINNER_DELIMITER));
 
-        System.out.println("최종 우승자 : " + winnerNames);
+        System.out.println(WINNER_ANNOUNCEMENT_PREFIX + winnerNames);
     }
 
     public void printErrorMessage(String message) {
-        System.out.println("[ERROR] " + message);
+        System.out.println(ERROR_PREFIX + message);
     }
 }
